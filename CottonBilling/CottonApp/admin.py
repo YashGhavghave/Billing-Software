ec891f13655e7d6155dc94a43cbd9b84d85e6d71
YashGhavghave <yashghavghave123@gmail.com>
2025-10-29 12:21:43 +0530

    Billing Software

---
+++ CottonBilling/CottonApp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+
+
+admin.site.site_header = "Cotton Billing Admin"
+admin.site.site_title = "Cotton Billing Admin Portal"
+admin.site.index_title = "Welcome to Cotton Billing Admin Portal"
---
+++ CottonBilling/CottonApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CottonappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'CottonApp'
